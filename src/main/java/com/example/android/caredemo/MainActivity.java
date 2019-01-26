9d876b35e2b0cb39d3d246608c4d48b1c865e8c5
Wendy Ide <ide.s.wendy@gmail.com>
2019-01-26 14:02:00 -0500

    adding relevant java packages

--- src/main/java/com/example/android/caredemo/MainActivity.java
+++ src/main/java/com/example/android/caredemo/MainActivity.java
@@ -12,6 +12,14 @@ import com.keenresearch.keenasr.KASRResult;
 import com.keenresearch.keenasr.KASRRecognizerListener;
 import com.keenresearch.keenasr.KASRBundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends EvsBaseActivity {
 
     @Override
